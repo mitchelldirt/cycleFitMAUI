ec04ce4416e93a8fa5d05779f4f2ae8109b5b1ee
mitchellm <developer@mitchellmudd.dev>
2023-08-09 08:12:54 -0400

    Can save HR data now. Disabled > readonly inputs.

--- cycleFitMAUI/wwwroot/domHelper.js
+++ cycleFitMAUI/wwwroot/domHelper.js
@@ -1,25 +1,24 @@
 ﻿window.domHelper = {
-    removeDisabledAttribute: function () {
-        var disabledInputs = document.querySelectorAll('input[disabled]');
-        disabledInputs.forEach(function (input) {
-            input.removeAttribute('disabled');
+    removeReadOnlyAttribute: function () {
+        var readonlyInputs = document.querySelectorAll('input[readonly]');
+        readonlyInputs.forEach(function (input) {
+            input.removeAttribute('readonly');
         });
     },
     
     addHeartRateData: function (HRData) {
         var HRInputs = document.getElementsByName('HR');
-
         // Maybe you should actually make the HR data just an array for simplicity so it's easier to apply to the inputs?
-        HRInputs[0] = HRData.Zone1.Low;
-        HRInputs[1] = HRData.Zone1.High;
-        HRInputs[2] = HRData.Zone2.Low;
-        HRInputs[3] = HRData.Zone2.High;
-        HRInputs[4] = HRData.Zone3.Low;
-        HRInputs[5] = HRData.Zone3.High;
-        HRInputs[6] = HRData.Zone4.Low;
-        HRInputs[7] = HRData.Zone4.High;
-        HRInputs[8] = HRData.Zone5.Low;
-        HRInputs[9] = HRData.Zone5.High; 
+        HRInputs[0].value = HRData.zone1.low;
+        HRInputs[1].value = HRData.zone1.high;
+        HRInputs[2].value = HRData.zone2.low;
+        HRInputs[3].value = HRData.zone2.high;
+        HRInputs[4].value = HRData.zone3.low;
+        HRInputs[5].value = HRData.zone3.high;
+        HRInputs[6].value = HRData.zone4.low;
+        HRInputs[7].value = HRData.zone4.high;
+        HRInputs[8].value = HRData.zone5.low;
+        HRInputs[9].value = HRData.zone5.high; 
     },
 
     getHRData: function () {
@@ -27,24 +26,24 @@
        
         return JSON.stringify({
             Zone1: {
-                Low: heartRates[0].value,
-                High: heartRates[1].value
+                Low: Number(heartRates[0].value),
+                High: Number(heartRates[1].value)
             },
             Zone2: {
-                Low: heartRates[2].value,
-                High: heartRates[3].value
+                Low: Number(heartRates[2].value),
+                    High: Number(heartRates[3].value)
             },
             Zone3: {
-                Low: heartRates[4].value,
-                High: heartRates[5].value
+                Low: Number(heartRates[4].value),
+                    High: Number(heartRates[5].value)
             },
             Zone4: {
-                Low: heartRates[6].value,
-                High: heartRates[7].value
+                Low: Number(heartRates[6].value),
+                    High: Number(heartRates[7].value)
             },
             Zone5: {
-                Low: heartRates[8].value,
-                High: heartRates[9].value
+                Low: Number(heartRates[8].value),
+                    High: Number(heartRates[9].value)
             }
         })
     
