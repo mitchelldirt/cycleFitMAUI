316026c318ddc50389be64af22b36be84bcb91ce
mitchellm <developer@mitchellmudd.dev>
2023-08-21 14:47:42 -0400

    Got first sample workout done, don't use recovery intensity! lol

--- cycleFitMAUI/Data/SampleWorkouts.cs
+++ cycleFitMAUI/Data/SampleWorkouts.cs
@@ -4,6 +4,7 @@ namespace cycleFitMAUI.Data;
 
 public class SampleWorkouts
 {
+    // Do not use Intensity.Recovery
     public static void FourEightMinute(HeartRateZones hrZones)
     {
         List<WorkoutStep> steps = new List<WorkoutStep>();
@@ -11,8 +12,81 @@ public class SampleWorkouts
         steps.Add(new WorkoutStep
         {
             DurationType = WktStepDuration.Time,
-            DurationValue = 10000,
+            DurationValue = 900000,
             Intensity = Intensity.Warmup,
+            TargetType = WktStepTarget.HeartRate,
+            Zone = hrZones.Zone2
+        });
+        // Repeats start here
+        steps.Add(new WorkoutStep
+        {
+            DurationType = WktStepDuration.Time,
+            DurationValue = 480000,
+            Intensity = Intensity.Active,
+            TargetType = WktStepTarget.HeartRate,
+            Zone = hrZones.Zone4
+        });
+        steps.Add(new WorkoutStep
+        {
+            DurationType = WktStepDuration.Time,
+            DurationValue = 240000,
+            Intensity = Intensity.Rest,
+            TargetType = WktStepTarget.HeartRate,
+            Zone = hrZones.Zone1
+        });
+        steps.Add(new WorkoutStep
+        {
+            DurationType = WktStepDuration.Time,
+            DurationValue = 480000,
+            Intensity = Intensity.Active,
+            TargetType = WktStepTarget.HeartRate,
+            Zone = hrZones.Zone4
+        });
+        steps.Add(new WorkoutStep
+        {
+            DurationType = WktStepDuration.Time,
+            DurationValue = 240000,
+            Intensity = Intensity.Rest,
+            TargetType = WktStepTarget.HeartRate,
+            Zone = hrZones.Zone1
+        });
+        steps.Add(new WorkoutStep
+        {
+            DurationType = WktStepDuration.Time,
+            DurationValue = 480000,
+            Intensity = Intensity.Active,
+            TargetType = WktStepTarget.HeartRate,
+            Zone = hrZones.Zone4
+        });
+        steps.Add(new WorkoutStep
+        {
+            DurationType = WktStepDuration.Time,
+            DurationValue = 240000,
+            Intensity = Intensity.Rest,
+            TargetType = WktStepTarget.HeartRate,
+            Zone = hrZones.Zone1
+        });
+        steps.Add(new WorkoutStep
+        {
+            DurationType = WktStepDuration.Time,
+            DurationValue = 480000,
+            Intensity = Intensity.Active,
+            TargetType = WktStepTarget.HeartRate,
+            Zone = hrZones.Zone4
+        });
+        steps.Add(new WorkoutStep
+        {
+            DurationType = WktStepDuration.Time,
+            DurationValue = 240000,
+            Intensity = Intensity.Rest,
+            TargetType = WktStepTarget.HeartRate,
+            Zone = hrZones.Zone1
+        });
+        steps.Add(new WorkoutStep
+        {
+            DurationType = WktStepDuration.Time,
+            DurationValue = 900000,
+            Intensity = Intensity.Cooldown,
             TargetType = WktStepTarget.HeartRate,
             Zone = hrZones.Zone2
         });
